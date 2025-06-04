0d01639f7d74d380b982596b5407b3abc5159e1c
EC2 Default User <ec2-user@ip-172-31-31-125.ec2.internal>
2025-06-04 02:27:16 +0000

    Homwork_12

---
+++ Homework-12/input.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+
+read -p "Enter your name: " name
+read -p "Enter your age: " age
+read -p "Enter your favorite language: " fav_lang
+read -p "Enter your hobby: " hobby
+read -p "Enter your dream: " dream
+
+
+
+echo "Hi, my name is $name, and I'm $age years old. Every morning, $name likes to spend time $hobby.
+As a $age-year-old, I believe it's never too late to follow your dreams. My favorite programming language is $fav_lang.
+I code in $fav_lang almost every day!
+One day, I hope my love for $fav_lang will help me achieve my dream of $dream.
+When I'm not busy working toward $dream, you'll find me $hobby to relax.
+That's a little about me, $name — a $age-year-old who loves $fav_lang, enjoys $hobby, and dreams of $dream!"
